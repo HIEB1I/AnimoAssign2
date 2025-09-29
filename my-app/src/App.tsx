f676c5fea0f552fa3ab5df384c558b7c5fcc3e5a
ShamenRos <charmaine_rosal@dlsu.edu.ph>
2025-09-29 23:23:01 +0800

    Resolve stash conflict; move FAC_Overview and route changes to cha_branch

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,34 @@
-//ADMIN DASHBOARD
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 
 // pages
 import ADMIN_Screen from "./ADMIN_Screen";
 import ADMIN_Inbox from "./ADMIN_Inbox";
+import STUDENT_Screen from "./STUDENT_Screen";
+import FAC_Overview from "./FAC_Overview";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Default redirect */}
-        <Route path="/" element={<Navigate to="/admin/screen" replace />} />
+        <Route path="/" element={<Navigate to="/faculty/overview" replace />} />
+
+        {/* Faculty routes */}
+        <Route path="/faculty/overview" element={<FAC_Overview />} />
 
         {/* Admin routes */}
         <Route path="/admin/screen" element={<ADMIN_Screen />} />
         <Route path="/admin/inbox" element={<ADMIN_Inbox />} />
 
+        {/* Student routes */}
+        <Route path="/student/screen" element={<STUDENT_Screen />} />
+
         {/* Login */}
         <Route path="/login" element={<Login />} />
 
-        {/* Catch-all redirect */}
-        <Route path="*" element={<Navigate to="/admin/screen" replace />} />
+        {/* Catch-all redirect (send unknown paths to faculty overview) */}
+        <Route path="*" element={<Navigate to="/faculty/overview" replace />} />
       </Routes>
     </BrowserRouter>
   );
